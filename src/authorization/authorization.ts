9436c02fe73baae0f8753baa237cadf91243146d
Leshkinson <lopatkool93@gmail.com>
2022-06-22 15:55:29 +0300

    fix basic validation commit

--- src/authorization/authorization.ts
+++ src/authorization/authorization.ts
@@ -2,11 +2,15 @@ import {Request, Response, NextFunction} from "express";
 
 const LOGIN = 'admin';
 const PASSWORD = 'qwerty';
-
+const TRUEPassword = 'Basic YWRtaW46cXdlcnR5';
 
 export const basicAuthorization = (req: Request, res: Response, next: NextFunction) => {
     const authorization: string | undefined = req.headers.authorization;
     if (!authorization) {
+        res.sendStatus(401)
+        return;
+    }
+    if (authorization !== TRUEPassword) {
         res.sendStatus(401)
         return;
     }
